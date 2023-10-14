4dec60ad4e18dfa0e2b8e6922240497c923b5d4f
felix2680 <feliciano.hernandez@alumno.udg.mx>
2023-10-14 02:02:33 -0600

    comenzé la codificiación de la clase

--- src/com/analizadores/AnalizadorSintactico.java
+++ src/com/analizadores/AnalizadorSintactico.java
@@ -1,6 +1,27 @@
-
 package com.analizadores;
 
 public class AnalizadorSintactico {
+
+    public static void funcion() throws GrammarException {
+        tipo_retorno();
+        identificador();
+        coincidir("");
+    }
+    public static void tipo_retorno()throws GrammarException{
+        
+    }
     
+    public static void identificador()throws GrammarException{
+        
+    }
+    public static void coincidir(String caracter){
+        
+    }
+    class GrammarException extends Exception {
+
+        public GrammarException() {
+            super("Cadena no válida");
+        }
+    }
+
 }
